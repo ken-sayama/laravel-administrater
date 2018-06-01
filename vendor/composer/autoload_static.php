ebff2d804cb0b26ca2bc07f5515617d68c6560d9
ken-sayama <sayamaken0402@gmail.com>
2018-06-01 11:51:52 +0900

    create model and migration file

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -333,6 +333,7 @@ class ComposerStaticInitad0bc92aa1d3a46e18c031ca705449f3
         'App\\Http\\Middleware\\TrimStrings' => __DIR__ . '/../..' . '/app/Http/Middleware/TrimStrings.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Models\\Admins\\Admin' => __DIR__ . '/../..' . '/app/Models/Admins/Admin.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
